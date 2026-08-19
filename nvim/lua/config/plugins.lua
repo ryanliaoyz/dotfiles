f4529d5b0a0443e3b50e60530523a1a239a78664
Ryan(Yunze) Liao <liaoyunze@hotmail.com>
2026-08-19 14:57:43 +0800

    upgrade nvim treesitter, add markview

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -13,12 +13,20 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   -- Treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
+    {
+      "nvim-treesitter/nvim-treesitter",
+      branch = "main",
+      lazy = false,
+      build = ":TSUpdate",
+
+      config = function()
+        local ts = require("nvim-treesitter")
+
+        ts.setup({
+          install_dir = vim.fn.stdpath("data") .. "/site",
+        })
+
+        ts.install({
           "lua",
           "vim",
           "vimdoc",
@@ -29,13 +37,17 @@ require("lazy").setup({
           "haskell",
           "python",
           "markdown",
-          "markdown_inline"
+          "markdown_inline",
+          "latex",
+        })
 
-        },
-        highlight = { enable = true },
-      })
-    end,
-  },
+        vim.api.nvim_create_autocmd("FileType", {
+          callback = function()
+            pcall(vim.treesitter.start)
+          end,
+        })
+      end,
+    },
 
   -- LSP core (we’re still using vim.lsp.enable, but this stays ready)
   { "neovim/nvim-lspconfig" },
@@ -112,6 +124,12 @@ require("lazy").setup({
     config = function()
       vim.cmd.colorscheme("tokyonight")
     end,
+  },
+
+  -- Markdown / Latex preview
+  {
+      "OXY2DEV/markview.nvim",
+      lazy = false,
   },
 
   -- Quick Comment: gcc/gbc/gc5j...
